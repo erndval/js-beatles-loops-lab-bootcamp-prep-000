863ebcc8cde9ddbbab4737e5b22f6cdc68b1c0b6
Ernie Val <erndval@gmail.com>
2019-02-26 18:33:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var blank = {}
   for (let i = 1, i < 4, i++) {
-    blank = [...blank, '${musicians} plays {instruments}.']
+    blank = [...blank, `${musicians} plays {instruments}.`]
   }
 }
